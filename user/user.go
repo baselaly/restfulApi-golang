310d265ddd801cb4e31308837aab29d9fc788717
basel <basel.3ly@gmail.com>
2020-05-01 04:51:39 +0300

    fix model struct

--- user/user.go
+++ user/user.go
@@ -1,14 +1,15 @@
 package user
 
 import (
-	"github.com/jinzhu/gorm"
+	"time"
 )
 
 // User model
 type User struct {
-	gorm.Model
-	Name     string `json:"name"`
-	Age      int    `json:"age"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID        int16     `json:"id" gorm:"primary_key"`
+	Email     string    `json:"email" gorm:"not null;unique"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
--- user/user_dto.go
+++
@@ -1,9 +0,0 @@
-package user
-
-type UserDto struct {
-	ID       uint   `json:"id"`
-	Name     string `json:"name"`
-	Age      int    `json:"age"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-}
